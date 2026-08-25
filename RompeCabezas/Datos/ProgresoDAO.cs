111d382d5cc10b339f486553c5279f081ba139fd
Jhon <alejandrotareas029@gmail.com>
2026-08-25 16:28:13 -0500

    v0.0 Creacion de las capas con datos vacios, tambien se agrego los datos y consultas para el progreso del usuario

---
+++ RompeCabezas/Datos/ProgresoDAO.cs
@@ -0,0 +1,70 @@
+﻿using RompeCabezas.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Web;
+
+namespace RompeCabezas.Datos
+{
+    public class ProgresoDAO
+    {
+        private readonly ConexionDB conexionDB;
+
+        public void GuardarProgreso(Progreso progreso)
+        {
+            using (SqlConnection conn = conexionDB.MtAbrirConexion())
+            {
+                conn.Open();
+
+                string consulta = @"insert into Progreso
+                              (FechaCompletado, TiempoSegundos, Mensaje) 
+                               VALUES
+                              (@fecha, @tiempo, @mensaje)";
+
+
+                SqlCommand cmd = new SqlCommand(consulta, conn);
+                cmd.Parameters.AddWithValue("@fecha", progreso.FechaCompletado);
+                cmd.Parameters.AddWithValue("@tiempo", progreso.TiempoSegundos);
+                cmd.Parameters.AddWithValue("@mensaje", progreso.Mensaje);
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+
+        public Progreso ObtenerUltimoProgreso()
+        {
+            Progreso progreso = null;
+
+            using (SqlConnection conn = conexionDB.MtAbrirConexion())
+            {
+                conn.Open();
+
+                string consulta = @"select top 1
+                                    Id,
+                                    FechaCompletado,
+                                    TiempoSegundos,
+                                    Mensaje
+                                    from Progreso
+                                    order by Id desc";
+
+                SqlCommand cmd = new SqlCommand(consulta, conn);
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    progreso = new Progreso()
+                    {
+                        Id = (int)dr["Id"],
+                        FechaCompletado = (DateTime)dr["FechaCompletado"],
+                        TiempoSegundos = (int)dr["TiempoSegundos"],
+                        Mensaje = dr["Mensaje"].ToString()
+                    };
+                }
+
+                return progreso;
+            }
+        }
+    }
+}
---
+++ RompeCabezas/Modelo/Progreso.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RompeCabezas.Modelo
+{
+    public class Progreso
+    {
+        public int Id { get; set; }
+        public DateTime FechaCompletado { get; set; }
+        public int TiempoSegundos { get; set; }
+        public string Mensaje { get; set; }
+    }
+}
