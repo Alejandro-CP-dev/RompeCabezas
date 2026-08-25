9d5251551b6862e23afaad46f180636a8d35ad9f
Jhon <alejandrotareas029@gmail.com>
2026-08-25 15:46:10 -0500

    v0.0_Creacion Principal de regalo de aniversario para pareja, primera parte, creacion por capas

---
+++ RompeCabezas/Datos/ConexionDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace RompeCabezas.Datos
+{
+    public class ConexionDB
+    {
+        private readonly string connexion = ConfigurationManager.ConnectionStrings["dbConexion"].ConnectionString;
+
+        public SqlConnection MtAbrirConexion()
+        {
+            return new SqlConnection(connexion);
+        }
+    }
+}
